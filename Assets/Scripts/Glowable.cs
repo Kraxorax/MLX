e024f60bad27402339ba4d8fe50c43889bd490c3
Kraxorax <gmarkokralj@gmail.com>
2023-08-11 17:12:25 +0200

    Simple hover

---
+++ Assets/Scripts/Glowable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+	public class Glowable : MonoBehaviour
+	{
+		Renderer oneWhoRenders;
+
+		// Use this for initialization
+		void Start()
+		{
+			oneWhoRenders = GetComponent<Renderer>();
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+
+		private Color startcolor;
+		void OnMouseEnter()
+		{
+			startcolor = oneWhoRenders.material.color;
+			oneWhoRenders.material.color = Color.yellow;
+		}
+		void OnMouseExit()
+		{
+			oneWhoRenders.material.color = startcolor;
+		}
+	}
+}
